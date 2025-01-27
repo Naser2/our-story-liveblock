5aaaf5d144dad3e37ec49dd940c0f4337cc81f74
Nas <nassersanou23@gmail.com>
2025-01-27 08:55:22 +0100

    Fouth Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const session = await auth();
+  console.log("NEXT_AUTH_SESSION: " + session)
   return (
     <ConvexClientProvider>
       <html lang="en">
